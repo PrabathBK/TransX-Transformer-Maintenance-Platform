a2d3871d021ef9923986d9129bc4b44e5d34b4f0
jaliyanimanthako <jaliyanimantha070@gmail.com>
2025-08-24 00:44:24 +0530

    Port Changed

--- transformer-inspector/frontend/vite.config.ts
+++ transformer-inspector/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Change this to your desired port number
+    port: 5173, // Change this to your desired port number
     host: true, // Allow external connections
   },
 })
